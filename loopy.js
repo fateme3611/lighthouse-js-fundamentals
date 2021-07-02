8a4fd0520f9e9410924dc1f29616e4818fdfa721
fateme <fateme3611@yahoo.com>
2021-07-02 18:12:52 -0400

    numbers between 100-200 with 3 exceptions

---
+++ loopy.js
@@ -0,0 +1,15 @@
+function range(){
+for(let i = 100; i<=200; i++){
+    if (i%3===0 & i%4===0){
+        console.log("LoopyLighthouse");
+    }
+    else if(i%4===0){
+        console.log("Lighthouse");
+    }
+    else if(i%3===0){
+        console.log ("loopy");
+    }
+    else console.log(i);
+    }
+}
+range();
