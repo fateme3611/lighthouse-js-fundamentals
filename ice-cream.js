7ecc042401dd4feac3fc40541b3efd30c99a9196
fateme <fateme3611@yahoo.com>
2021-07-02 18:11:29 -0400

    numbers between 100-200 with 3 exceptions

--- ice-cream.js
+++ ice-cream.js
@@ -1,4 +1,4 @@
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
 
 const newArray = iceCreamFlavours.map(function (item){
@@ -6,14 +6,19 @@ const newArray = iceCreamFlavours.map(function (item){
 });
 
 
-const newArray2 = iceCreamFlavours.map((item)=>{
-    return item;
-});
+// const newArray2 = iceCreamFlavours.map((item)=>{
+//     return item;
+// });
 
 
-const newArray3 = iceCreamFlavours.map((item)=>item);
+//const newArray3 = iceCreamFlavours.map((item)=>item);
 
-const newArray4 = [...iceCreamFlavours];
+// const newArray4 = [...iceCreamFlavours];
 
 newArray.push("root beer");
 console.log(newArray);
+
+console.log(iceCreamFlavours[0]);
+console.log(newArray[newArray.length-1]);
+console.log(newArray.length);
+
